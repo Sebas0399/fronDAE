1cd37bc93d9f02dcb2fd0eb40a15cd7e06833f6b
Sebas0399 <antony.aguay@gmail.com>
2024-03-21 15:54:23 -0500

    fix

--- src/modules/pagos/pages/Carrito.vue
+++ src/modules/pagos/pages/Carrito.vue
@@ -18,18 +18,25 @@
 
 <script>
 import axios from "axios";
+import { ref, toRaw } from 'vue'
 
 import jsonData from "@/modules/pagos/helpers/planes.json"
-import { pagarFachada } from '../helpers/pago'
 import cedulaServiceInstance from "@/modules/utils/tokenUtils"
 export default {
     data() {
         return {
+            algo: 10,
             planes: jsonData,
             precio: null,
             appId: "4zNC2K4akUCw6ta2oFJ6g",
             token: "moZXbXlCPgo5VhaoTdx3MEMoJ2tWopWsAGIxPSpWDwvC-VflrgHR5f642UPVEZt-DYONxE1NWzvhjxH1vuuUf4cO8ZrEzb8FkB0YmPiSHQHq-rvkFXv_12_8xPAetHD48yJh4gRiGMuvvXWijnrv9hUE_wr7rqT1v1SYYOcStbdAblhokAfOMtYakmE8rfLE8DsvF-h-wIf-QFacHqRca4Iaou1ignVqNJz8HA1Yb6GIhuKVdJZpjrGdhbcFyyGoE58Ewbo79swhrCgMb4zfvkY_5WuMkdY5lmPhcfOouwAJMZ7U74wbH_FSS28R8xfmvEpuD4xtgptKvYFMEXEQyucXkGE"
-
+            , transactionData: {
+                amount: 100,
+                amountWithoutTax: 100,
+                currency: 'USD',
+                clientTransactionId: 'unique_identifier', // Replace with a unique identifier for your transaction
+                lang: 'es' // Language (optional)
+            }
         }
     },
     mounted() {
@@ -39,13 +46,6 @@ export default {
     },
     methods: {
         async initPayphoneButton() {
-            this.precio = {
-                "amount": this.planes[this.$route.params.plan - 1].precio + this.planes[this.$route.params.plan - 1].precio * 0.13,
-                "tax": this.planes[this.$route.params.plan - 1].precio * 0.13,
-                "amountWithTax": this.planes[this.$route.params.plan - 1].precio,
-                "clientTransactionId": cedulaServiceInstance.getCedula() + 1,
-                "currency": "USD"
-            }
             try {
                 // Load the Payphone script dynamically (recommended for security)
                 const script = document.createElement('script');
@@ -61,7 +61,13 @@ export default {
                     btnHorizontal: true,
                     btnCard: true,
                     createOrder: async function (actions) {
-                        return actions.prepare(this.precio).then(function (paramlog) {
+                        return actions.prepare({
+                            amount: 1000,
+                            amountWithoutTax: 1000,
+                            currency: "USD",
+                            clientTransactionId: "identificador-único",
+                            lang: "es"
+                        }).then(function (paramlog) {
                             console.log("bien")
                             console.log(paramlog);
                             return paramlog;
@@ -76,7 +82,7 @@ export default {
                         console.log('Payment complete:', model);
                         // Handle successful payment completion (e.g., update order status, display confirmation message)
                     },
-                    onabort:function(){
+                    onabort: function () {
 
                     }
                 }).render('#pp-button-container');
@@ -84,18 +90,7 @@ export default {
                 console.error('Error initializing Payphone Button:', error);
             }
         },
-        pagar() {
-            this.precio = {
-                "amount": this.planes[this.$route.params.plan - 1].precio + this.planes[this.$route.params.plan - 1].precio * 0.13,
-                "tax": this.planes[this.$route.params.plan - 1].precio * 0.13,
-                "amountWithTax": this.planes[this.$route.params.plan - 1].precio,
-                "clientTransactionId": cedulaServiceInstance.getCedula() + 1,
-                "currency": "USD"
-            }
-            pagarFachada(this.precio).then((res) => {
-                console.log(res)
-            })
-        }
+
     },
 
 }
