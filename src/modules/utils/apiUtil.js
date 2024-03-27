a494f6f781219d39806f2284d6978912b3f6afe7
Sebas0399 <antony.aguay@gmail.com>
2024-03-27 16:25:44 -0500

    fix

--- src/modules/utils/apiUtil.js
+++ src/modules/utils/apiUtil.js
@@ -1,6 +1,6 @@
 //const API_URL = "https://daeapi.azurewebsites.net";
-//const API_URL="https://dae-6nst.onrender.com"
-const API_URL = "http://localhost:5000";
+const API_URL="https://dae-6nst.onrender.com"
+//const API_URL = "http://localhost:5000";
 
 export function getApiUrl(endpoint) {
     return `${API_URL}/${endpoint}`;
